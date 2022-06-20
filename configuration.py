53bd26a5ab7e6f9a7161120094caba6b6bc4de8a
nouhaylaeraysy <96634977+nouhaylaeraysy@users.noreply.github.com>
2022-06-20 20:10:00 +0200

    Update and rename data_to_csv.py to configuration.py

---
+++ configuration.py
@@ -0,0 +1,28 @@
+
+from configparser import ConfigParser
+
+#Get the configparser object
+config_object = ConfigParser()
+
+
+
+    #Read config.ini file
+config_object = ConfigParser()
+config_object.read("config.ini")   
+
+#Get the password
+
+
+def getConfigBySection(section,keye):
+    try:
+        value = config_object.get(section, keye)
+        return value
+    except ConfigParser.NoOptionError:
+        return 0
+
+    
+
+
+
+
+
