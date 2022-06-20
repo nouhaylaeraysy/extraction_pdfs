d9e9dba08a90f1fab6e225da2cedd616dd51b83a
nouhaylaeraysy <96634977+nouhaylaeraysy@users.noreply.github.com>
2022-06-20 20:06:12 +0200

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,8 +1,10 @@
 import pdfplumber
 import re  
-code = ''
 import collections
 import string, sys
+
+
+
 
 def parser(file):
     my_dico = {   # lines starts with decimal
@@ -11,16 +13,12 @@ def parser(file):
         'Categorie ':'Purpose',
         ' Système(s) d’évaluation et de vérification': 'System for assessment and verification of the constancy of performance',
         'Declared performance': 'Declared performance',
-        'Specific technical characteristics': 'Specific technical characteristics',
-   
-
+        'Specific technical characteristics': 'Specific technical characteristics'
     }
-
     my_dico_list = {   # another instruction for informations into this lines 
         'Declared performance': 'Declared performance',
-        'Specific technical characteristics': 'Specific technical characteristics',
+        'Specific technical characteristics': 'Specific technical characteristics'
         }
-
     my_dico2 = {
         'Harmonised standard': 'Norme',
         'Number of layers': 'Number of layers',
@@ -32,7 +30,6 @@ def parser(file):
     }
 
     data = {'Nom du Fichier ' : file , 'Nom de Produit' :'' }  # stock keys with their values 
-
     def get_key(val):   # retourne  clé et sa valeur  dans une liste 
         for key, value in my_dico.items():
             if val == value:
@@ -46,20 +43,14 @@ def parser(file):
                 return key
 
         return 0
-  
     test = {}
     test_list = {}
-
-
     with pdfplumber.open(file) as pdf:
-    
         allTexts = ''
         for i in range(len(pdf.pages)):
             page = pdf.pages[i]
             text = page.extract_text()
             allTexts = allTexts+text   # texte de l'ensemble des pages 
-
-
     my_list = []
 
     for row in allTexts.split('\n'):   # chaque fois on a retour a la linge  => nouvelle ligne 
@@ -70,40 +61,29 @@ def parser(file):
     for index,row1 in enumerate(my_list):
         if row1 == 'Declaration of performance':
             data['Nom de Produit'] = my_list[index +2 ]
-
-
     regex = re.compile(r'(^\d.$)') # pour 6.
 
     for index, row1 in enumerate(my_list) :
         if regex.match(row1):
             a = index
             test_list[index] = 'none'  # test_list va prendre l'indice 15   de 6.
-
-
     for index, row1 in enumerate(my_list) :
         for i, s in enumerate(my_dico.values()):
             if (s in row1.strip()):
                 key = get_key(s)
                 test[index] =  row1.strip()   ##### la premiere partie de pdf 
-
         for i, s in enumerate(my_dico_list.values()):
             if (s in row1.strip()):
                 key = get_key(s)
                 test_list[index] =  row1.strip()  ##### prend l'indice correspond a la ligne 7... et 8...
-   
-
     dictionary_items = test.items()
     test = collections.OrderedDict(sorted(test.items()))##### assosier chaque indice a sa valeur dans le dictionnaire 
     atest = {}   # prend les valeurs de test 
     for k, v in test.items(): 
         atest[k]= v  
-
     test = atest
-
     testvalues = (test.keys())
-
     t = len(testvalues)   ### t = la longueur de liste test qui prend les  premiere valeurs 7
-
     for k, value in enumerate(testvalues) :
         if(k+1 <= t-1):
             keyend = k+1
@@ -144,13 +124,10 @@ def parser(file):
                                            data[s2] = data[z2] + ' ' +y[1].strip()
                                         else:
                                            data[s2] = y[1].strip()
-
-
     for value in my_list:
         if value.startswith('Eberhardzell,'):
             data['Date D\'émission '] =  value.split()[-1]
-
-    
+   
     return(data)
 
 
